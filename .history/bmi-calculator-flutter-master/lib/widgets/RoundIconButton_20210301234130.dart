fa9693d4358042ee171a4f3f2b8883953e448f15
Pial, Hamidur Rahaman <pial0007l@gmail.com>
2021-03-01 23:41:50 +1030

    new round icon widget file created

---
+++ .history/bmi-calculator-flutter-master/lib/widgets/RoundIconButton_20210301234130.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  const RoundIconButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
