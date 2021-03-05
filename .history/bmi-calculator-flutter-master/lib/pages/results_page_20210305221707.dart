0eb9c1a7e56e73bd8de26461a4a6e17ab672dcff
Pial, Hamidur Rahaman <pial0007l@gmail.com>
2021-03-05 22:18:28 +1030

    body status enum created

--- .history/bmi-calculator-flutter-master/lib/pages/results_page_20210305221707.dart
+++ .history/bmi-calculator-flutter-master/lib/pages/results_page_20210305221707.dart
@@ -3,13 +3,8 @@ import 'package:bmi_calculator/widgets/reusableCard.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-enum BodyStatus {
-  NORMAL,
-  OVERWEIGHT,
-  UNDERWEIGHT,
-}
-
 class ResultsPage extends StatelessWidget {
+  ,
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- .history/bmi-calculator-flutter-master/lib/pages/results_page_20210305221735.dart
+++ .history/bmi-calculator-flutter-master/lib/pages/results_page_20210305221735.dart
@@ -3,13 +3,12 @@ import 'package:bmi_calculator/widgets/reusableCard.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-enum BodyStatus {
-  NORMAL,
-  OVERWEIGHT,
-  UNDERWEIGHT,
+enum BodyStatus{
+  
 }
 
 class ResultsPage extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
